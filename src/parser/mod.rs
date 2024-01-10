f474c26c6c99296aa95743df274ca541e8f53569
chentianyi <chentyit@163.com>
2024-01-10 20:22:22 +0800

    initialize parser logic.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,1 +1,16 @@
+mod ast;
 pub mod parser;
+
+trait CommandAstNode {
+    fn name(&self) -> &str;
+    fn set_option(&mut self, option: &str, value: &str);
+    fn get_option(&self, option: &str) -> Option<&str>;
+}
+
+impl std::fmt::Debug for dyn CommandAstNode {
+    // Implement the Debug trait for CommandAstNode here
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // ...
+        Ok(())
+    }
+}
