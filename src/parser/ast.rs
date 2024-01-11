8f18c811818ddc32e81a372ea8422cd18b0f462e
chentianyi <chentyit@163.com>
2024-01-11 20:11:30 +0800

    Completed the rough design of the syntax parsing logic

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -4,14 +4,23 @@ use crate::token::token::Token;
 
 use super::CommandAstNode;
 
-struct LsCommand {
+pub struct LsCommand {
     token: Token,
     option: HashMap<String, String>,
-    _path: Vec<String>,
+    path: Vec<String>,
+}
+
+impl LsCommand {
+    pub fn new(token: Token) -> Self {
+        LsCommand {
+            token,
+            option: HashMap::new(),
+            path: Vec::new(),
+        }
+    }
 }
 
 impl CommandAstNode for LsCommand {
-
     fn name(&self) -> &str {
         self.token.literal()
     }
