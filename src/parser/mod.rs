36fb0701d40974275afe3e89d98430fdce696a05
Tianyi <chentyit@163.com>
2024-01-12 19:40:48 +0800

    Add the Debug trait inheritance to CommandAstNode and implement the Debug trait for its implementing structs.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,16 +1,10 @@
-mod ast;
+use std::collections::HashMap;
+
+pub mod ast;
 pub mod parser;
 
-trait CommandAstNode {
+pub trait CommandAstNode: std::fmt::Debug {
     fn name(&self) -> &str;
     fn set_option(&mut self, option: String, value: String);
     fn get_option(&self, option: &str) -> Option<&str>;
-}
-
-impl std::fmt::Debug for dyn CommandAstNode {
-    // Implement the Debug trait for CommandAstNode here
-    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // ...
-        Ok(())
-    }
 }
