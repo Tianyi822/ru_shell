6418f8afa0ae93e0d2c5c76c5f031809e8166143
Tianyi <chentyit@163.com>
2024-01-13 18:49:06 +0800

    Add the 'cd' command ast node.

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -38,3 +38,38 @@ impl CommandAstNode for LsCommand {
         self.value.push(value)
     }
 }
+
+#[derive(Debug)]
+pub struct CdCommand {
+    token: Token,
+    option: HashMap<String, String>,
+    value: Vec<String>,
+}
+
+impl CdCommand {
+    pub fn new(token: Token) -> Self {
+        CdCommand {
+            token,
+            option: HashMap::new(),
+            value: Vec::new(),
+        }
+    }
+}
+
+impl CommandAstNode for CdCommand {
+    fn name(&self) -> &str {
+        self.token.literal()
+    }
+
+    fn set_option(&mut self, option: String, value: String) {
+        self.option.insert(option, value);
+    }
+
+    fn get_option(&self, option: &str) -> Option<&str> {
+        self.option.get(option).map(|s| s.as_str())
+    }
+
+    fn add_value(&mut self, value: String) {
+        self.value.push(value)
+    }
+}
