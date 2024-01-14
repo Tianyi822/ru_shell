5f4bb28dea39ac9dec484969bedfea679e482bc8
chentianyi <chentyit@163.com>
2024-01-14 18:26:01 +0800

    Add the parsing logic for the 'cd' command and the judgment rule for the '~' token.

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -25,8 +25,15 @@ mod parser_test {
 
     #[test]
     fn test_new_parser() {
-        let parser = Parser::new("ls -l -h --tree --depth 3 Programs/Rust/ru-shell,Programs/Rust/ru-shell");
+        let parser = Parser::new("ls -l -h --tree --depth 3 ~/Programs/Rust/ru-shell,Programs/Rust/ru-shell");
 
         println!("{:#?}", parser);
+    }
+
+    #[test]
+    fn test_cd_command_parse() {
+        let p = Parser::new("cd ~/Programs/Rust/ru-shell,Programs/Rust/ru-shell");
+
+        println!("{:#?}", p);
     }
 }
