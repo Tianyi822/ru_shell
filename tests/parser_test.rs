508444459cbc26f97f108862a82daf056341ffba
chentianyi <chentyit@163.com>
2024-01-18 11:13:56 +0800

    Separate out parse_exe_cmd for parsing execution commands, and separate out parse_chain_cmd for parsing chain commands.

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -1,15 +1,15 @@
 #[cfg(test)]
 mod parser_test {
-    use ru_shell::parser::ast::{CdCommand, LsCommand};
+    use ru_shell::parser::ast::ExeCommand;
     use ru_shell::parser::parser::Parser;
-    use ru_shell::parser::{Command, ExeCommandAstNode};
+    use ru_shell::parser::Command;
     use ru_shell::token::token::{Token, TokenType};
 
     #[test]
     fn test_show_command() {
-        let mut command_ast: Vec<Box<dyn ExeCommandAstNode>> = Vec::new();
+        let mut command_ast: Vec<Box<dyn Command>> = Vec::new();
 
-        let mut ls_command = LsCommand::new(Token::new(TokenType::Ls, "ls".to_string()));
+        let mut ls_command = ExeCommand::new(Token::new(TokenType::Ls, "ls".to_string()));
         ls_command.set_options(vec![
             ("-l".to_string(), "".to_string()),
             ("-h".to_string(), "".to_string()),
@@ -20,7 +20,7 @@ mod parser_test {
 
         command_ast.push(Box::new(ls_command));
 
-        println!("{:#?}", command_ast);
+        // println!("{:#?}", command_ast);
     }
 
     #[test]
@@ -30,16 +30,15 @@ mod parser_test {
         );
 
         parser.iter().for_each(|command| {
-            let c = command.as_any().downcast_ref::<LsCommand>().unwrap();
-            assert_eq!(c.name(), "ls");
+            assert_eq!(command.name(), "ls");
             assert_eq!(
                 command.get_type(),
                 &ru_shell::parser::CommandType::ExtCommand
             );
-            assert_eq!(c.get_option("-l"), Some(""));
-            assert_eq!(c.get_option("-h"), Some(""));
-            assert_eq!(c.get_option("--tree"), Some(""));
-            assert_eq!(c.get_option("--depth"), Some("3"));
+            assert_eq!(command.get_option("-l"), Some(""));
+            assert_eq!(command.get_option("-h"), Some(""));
+            assert_eq!(command.get_option("--tree"), Some(""));
+            assert_eq!(command.get_option("--depth"), Some("3"));
         });
     }
 
@@ -48,8 +47,7 @@ mod parser_test {
         let parser = Parser::new("cd ~/Programs/Rust/ru-shell,Programs/Rust/ru-shell");
 
         parser.iter().for_each(|command| {
-            let c = command.as_any().downcast_ref::<CdCommand>().unwrap();
-            assert_eq!(c.name(), "cd");
+            assert_eq!(command.name(), "cd");
             assert_eq!(
                 command.get_type(),
                 &ru_shell::parser::CommandType::ExtCommand
