c56b23c27a4cbdc9f6034de53152377cea1508de
chentianyi <chentyit@163.com>
2024-01-21 19:54:15 +0800

    Design the logical of executor to analyze AST node.

--- src/executor/executor.rs
+++ src/executor/executor.rs
@@ -1,37 +1,53 @@
 use crate::executor::Command;
 
+use crate::parser::parser::ParserIterator;
 use crate::parser::{CommandAstNode, CommandType};
 
+// This executor obtains the commands to be executed
+// and their relevant parameters by parsing the AST,
+// saves the parsing results into an array,
+// and executes them in sequence immediately
+// after completing the parsing of all AST nodes.
 pub struct Executor {
+    // Commands to be executed
     cmds: Vec<Box<dyn Command>>,
 }
 
 impl Executor {
-    pub fn new() -> Self {
-        Self {
-            cmds: Vec::new()
+    // Create new Executor
+    pub fn new(parser_iter: ParserIterator) -> Self {
+        let mut executor = Self { cmds: Vec::new() };
+
+        // Analyze the AST and save the command into an array
+        for cmd in parser_iter {
+            executor.add_cmd(cmd);
         }
+
+        executor
     }
 
+    // Add command to cmds that was analyzed
     pub fn add_cmd(&mut self, cmd: Box<dyn CommandAstNode>) {
-        match cmd.get_type() {
+        let cmd = match cmd.get_type() {
             CommandType::ExtCommand => self.analyze_exe_node(cmd),
-            CommandType::ChainCommand => self.analyze_chain_node(cmd)
-        }
+            CommandType::ChainCommand => self.analyze_chain_node(cmd),
+        };
+
+        self.cmds.push(cmd);
     }
 
-    fn analyze_exe_node(&mut self, cmd: Box<dyn CommandAstNode>) {
+    /// Analyze the AST which type is [`parser::CommandType::ExtCommand`].
+    fn analyze_exe_node(&mut self, cmd: Box<dyn CommandAstNode>) -> Box<dyn Command> {
         match cmd.name() {
-            "ls" => {
-                todo!()
-            }
+            "ls" => self.analyze_ls_node(),
             _ => {
                 todo!()
             }
         }
     }
 
-    fn analyze_chain_node(&mut self, cmd: Box<dyn CommandAstNode>) {
+    /// Analyze the AST which type is [`parser::CommandType::ChainCommand`].
+    fn analyze_chain_node(&mut self, cmd: Box<dyn CommandAstNode>) -> Box<dyn Command> {
         match cmd.name() {
             "|" => {
                 todo!()
@@ -40,5 +56,10 @@ impl Executor {
                 todo!()
             }
         }
+    }
+
+    // Analyze the 'ls' command AST node.
+    fn analyze_ls_node(&self) -> Box<dyn Command> {
+        todo!()
     }
 }
--- src/executor/ls.rs
+++ src/executor/ls.rs
@@ -17,6 +17,22 @@ pub struct LsCmd {
 
     // set file or directory path
     path: Vec<PathBuf>,
+
+    // This field instruct the program what to do.
+    // 'ls'                     => status-0 : default status
+    // 'ls -l'                  => status-1 : show details of files and directories
+    // 'ls -a'                  => status-2 : show hidden files and directories
+    // 'ls -a -l'               => status-3 : calculated by 1 | 2, it will show details of all hidden files and directories
+    // 'ls -H'                  => status-4 : set status to 4, but do nothing, don't ask why, Linux ls command also do nothing when get '-h' option
+    // 'ls -l -H'               => status-5 : calculated by 1 | 4, it will show details of files and directories with human readable file sizes
+    // 'ls -a -l -H'            => status-7 : calculated by 1 | 2 | 4, it will show details of all hidden files and directories with human readable file sizes
+    // 'ls -t' of 'ls --tree'   => status-8 : show files and directories as a tree
+    // other command            => status-0 : default status
+    // Above status were set by the parse function what we implemented in the impl code block.
+    //
+    // Attention: You must use #[arg(skip)] to skip the hidden field,
+    // otherwise it will be shown in help message, and even panic will appear in the program!!!
+    status: u8,
 }
 
 impl LsCmd {
@@ -28,6 +44,7 @@ impl LsCmd {
             human_readable: false,
             resort: false,
             path: Vec::new(),
+            status: 0,
         }
     }
 }
