c7717a59dcd722455432952310540faeb6024b82
chentianyi <chentyit@163.com>
2024-01-24 19:41:51 +0800

    Add 'get_files_and_dirs' function for 'LsCmd' and pass the test case.

--- tests/executor_test.rs
+++ tests/executor_test.rs
@@ -4,9 +4,11 @@ mod executor_test {
 
     #[test]
     fn test_new_executor() {
-        let exe = Executor::new("ls -l -a");
+        let exe = Executor::new("ls -l -a ./,/Users/chentianyi/Program/Rust-workplace");
 
         assert_eq!(exe.get_cmds().len(), 1);
         println!("{:#?}", exe.get_cmds()[0]);
+
+        exe.execute();
     }
 }
