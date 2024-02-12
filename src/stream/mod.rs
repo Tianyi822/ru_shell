66ced8955956adc5962e7c0edcbe7e4e9e0aa195
Tianyi <chentyit@163.com>
2024-02-12 13:08:36 +0800

    Add stream to the each execution command to manage their result data/

---
+++ src/stream/mod.rs
@@ -0,0 +1,14 @@
+// This stream is used to output the result of the execution of the code.
+// Its directions include the console, file, and network.
+// 
+// For example, the console stream is used to output the result of the code execution to the console.
+// The file stream is used to output the result of the code execution to a file,
+// the type of the stream is determined by the way that the executor is called.
+// If use the REPL, it will create a console stream and transfer it to the executor,
+// but in pipeline command, it will create a pipeline stream for data source and data destination.
+//
+// In conclusion, each 'exe' command has a stream for outputting result, and the stream is passed in when creating the 'exe' command.
+// As for the result type of output and the implementation method, it's determined by the caller.
+pub trait Stream {
+    fn output(&self) -> String;
+}
