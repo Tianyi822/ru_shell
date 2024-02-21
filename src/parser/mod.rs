d9529b2e7cdb12466e28200219dfce56c624448c
Tianyi <chentyit@163.com>
2024-02-21 15:55:23 +0800

    Refactor the syntax parser, writing independent parsing logical for each command, ensuring that each command parsers its own options and their corresponding values.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,12 +3,15 @@ use std::rc::Rc;
 
 use crate::lexer::Lexer;
 use crate::parser::ast_node_trait::CommandAstNode;
-use crate::parser::cmds_ast_node::{ChainCommandAstNode, ExeCommandAstNode};
+
 use crate::token::token::Token;
 use crate::token::token::TokenType;
 
+use self::cmds_ast_node::ChainCommandAstNode;
+
 pub mod ast_node_trait;
 pub mod cmds_ast_node;
+mod parsing_func;
 
 // Since the syntax of command-line interfaces is simpler than that of programming languages,
 // this parser analyzes and processes in the order of tokens.
@@ -105,7 +108,7 @@ impl Parser {
             if self.cur_token.borrow().clone().token_type() == &TokenType::Eof {
                 return;
             }
-            
+
             // Check if the current token is a command.
             if !self.check_wether_is_command() {
                 self.collect_error("Invalid command");
@@ -114,7 +117,7 @@ impl Parser {
 
             // Parse the corresponding command based on the token type
             // and return the parsed AST (Abstract Syntax Tree) node.
-            let ast_node: Box<dyn CommandAstNode> = match self.parse_exe_cmd() {
+            let ast_node: Box<dyn CommandAstNode> = match self.parse_cmd() {
                 Some(ext_cmd) => ext_cmd,
                 None => break,
             };
@@ -161,14 +164,14 @@ impl Parser {
     }
 
     // Parse the command whose type is execute command.
-    fn parse_exe_cmd(&self) -> Option<Box<dyn CommandAstNode>> {
+    fn parse_cmd(&self) -> Option<Box<dyn CommandAstNode>> {
         let cur_token = self.cur_token.borrow().clone();
         // Parse the corresponding command based on the token type
         // and return the parsed AST (Abstract Syntax Tree) node.
         let ext_cmd: Option<Box<dyn CommandAstNode>> = match cur_token.token_type() {
-            TokenType::Ls | TokenType::Cd | TokenType::Grep | TokenType::Cat => {
-                Some(self.parse_exe_command())
-            }
+            TokenType::Ls => self.parse_ls_cmd(),
+            TokenType::Cat => self.parse_cat_cmd(),
+            TokenType::Grep => self.parse_grep_cmd(),
             _ => None,
         };
 
@@ -184,7 +187,7 @@ impl Parser {
             // Move to next Token to parse
             self.next_token();
             // Set data destination of chain command.
-            let destination = self.parse_exe_cmd();
+            let destination = self.parse_cmd();
             cmd.set_destination(destination);
 
             return Some(Box::new(cmd));
@@ -199,220 +202,6 @@ impl Parser {
             return true;
         }
         false
-    }
-
-    // Parse execute command
-    fn parse_exe_command(&self) -> Box<dyn CommandAstNode> {
-        // Build the exe command node.
-        let mut exe_command = ExeCommandAstNode::new(self.cur_token.borrow().clone());
-
-        self.next_token();
-
-        // Parse the parameters of the ls command.
-        match self.parse_options() {
-            Some(params) => {
-                exe_command.set_options(params);
-            }
-            None => (),
-        };
-
-        // Parse the pattern of the grep command.
-        if exe_command.token_type() == &TokenType::Grep {
-            match self.parse_pattern() {
-                Some(paths) => exe_command.add_value(paths),
-                None => {
-                    return Box::new(exe_command);
-                }
-            };
-        }
-
-        // Parse the paths of the ls command.
-        match self.parse_paths() {
-            Some(paths) => exe_command.set_values(paths),
-            None => {
-                if exe_command.token_type() == &TokenType::Grep {
-                    self.collect_error("Grep command needs a path");
-                    return Box::new(exe_command);
-                }
-            }
-        };
-
-        match self.parse_chain_cmd() {
-            Some(mut token) => {
-                token.set_source(Some(Box::new(exe_command)));
-                token
-            }
-            None => Box::new(exe_command),
-        }
-    }
-
-    // Parse the matching rules of the 'Pattern matching' command.
-    fn parse_pattern(&self) -> Option<String> {
-        // If the current token is a double quotation mark, then the pattern is complete.
-        if *self.cur_token.borrow().token_type() == TokenType::Quote {
-            self.next_token();
-        } else {
-            self.collect_error("Missing pattern. You can use `\"` to quote the pattern.");
-            return None;
-        }
-
-        // Iterate through the subsequent tokens and add them to the current pattern
-        // until the condition is not met.
-        let mut pattern = String::from("");
-        loop {
-            if *self.cur_token.borrow().token_type() == TokenType::Literal
-                || *self.cur_token.borrow().token_type() == TokenType::Num
-                || *self.cur_token.borrow().token_type() == TokenType::Slash
-                || *self.cur_token.borrow().token_type() == TokenType::Dot
-                || *self.cur_token.borrow().token_type() == TokenType::Tilde
-            {
-                pattern.push_str(self.cur_token.borrow().literal());
-                self.next_token();
-            } else {
-                break;
-            }
-        }
-
-        // If the current token is a double quotation mark, then the pattern is complete.
-        if *self.cur_token.borrow().token_type() == TokenType::Quote {
-            self.next_token();
-        } else {
-            self.collect_error("Invalid pattern, missing right quotation mark.");
-            return None;
-        }
-
-        Some(pattern)
-    }
-
-    // Parse the paths of the command.
-    fn parse_paths(&self) -> Option<Vec<String>> {
-        let mut paths: Vec<String> = Vec::new();
-
-        loop {
-            let cur_token = self.cur_token.borrow().clone();
-            match *cur_token.token_type() {
-                TokenType::Tilde
-                | TokenType::Literal
-                | TokenType::Num
-                | TokenType::Slash
-                | TokenType::Dot => {
-                    paths.push(self.parse_path().unwrap());
-                }
-                _ => break,
-            };
-
-            // Skip the comma and get next path.
-            // If the current token isn't comma, then break the loop.
-            if self.cur_token.borrow().clone().token_type() != &TokenType::Comma {
-                break;
-            } else {
-                self.next_token();
-            }
-        }
-
-        if paths.is_empty() {
-            return None;
-        } else {
-            return Some(paths);
-        }
-    }
-
-    // Parse the path of the command.
-    fn parse_path(&self) -> Option<String> {
-        let mut path = String::from(self.cur_token.borrow().literal());
-        self.next_token();
-
-        loop {
-            if self.lexer.peek_token().is_none() {
-                break;
-            }
-
-            if *self.cur_token.borrow().token_type() == TokenType::Literal
-                || *self.cur_token.borrow().token_type() == TokenType::Num
-                || *self.cur_token.borrow().token_type() == TokenType::Slash
-                || *self.cur_token.borrow().token_type() == TokenType::Dot
-                || *self.cur_token.borrow().token_type() == TokenType::Tilde
-            {
-                path.push_str(self.cur_token.borrow().literal());
-                self.next_token();
-            } else {
-                break;
-            }
-        }
-
-        Some(path)
-    }
-
-    // Parse the parameters of the command.
-    fn parse_options(&self) -> Option<Vec<(String, String)>> {
-        let mut params: Vec<(String, String)> = Vec::new();
-
-        loop {
-            // If the current token isn't a parameter, then break the loop.
-            let cur_token = self.cur_token.borrow().clone();
-            match cur_token.token_type() {
-                TokenType::ShortParam | TokenType::LongParam => {
-                    match self.parse_option() {
-                        Some((param, value)) => {
-                            params.push((param, value));
-                        }
-                        None => break,
-                    };
-                }
-                _ => break,
-            };
-        }
-
-        Some(params)
-    }
-
-    // Parse the parameters of the command.
-    fn parse_option(&self) -> Option<(String, String)> {
-        let cur_token = self.cur_token.borrow().clone();
-
-        // Get the parameter and its value.
-        if *cur_token.token_type() == TokenType::ShortParam
-            || *cur_token.token_type() == TokenType::LongParam
-        {
-            let param = cur_token.literal().to_string();
-
-            self.next_token();
-
-            let value = self.parse_param_value().unwrap_or_else(|| "".to_string());
-
-            return Some((param, value));
-        }
-
-        None
-    }
-
-    // Parse the value of the parameter.
-    // The reason why the value of the parameter is optional is that
-    // user can set the value like this:
-    // 1. ls -l                 : the '-l' parameter has no value.
-    // 2. ls --tree --depth 3   : the value '3' is assigned to the '--depth' parameter without '='.
-    // 3. ls --color=auto       : the value 'auto' is assigned to the '--color' parameter with '='.
-    // So it is necessary to check in what way the value is assigned to the parameter.
-    fn parse_param_value(&self) -> Option<String> {
-        let cur_token = self.cur_token.borrow().clone();
-
-        // Skip the assignment operator.
-        if *cur_token.token_type() == TokenType::Assignment {
-            self.next_token();
-        }
-
-        let cur_token = self.cur_token.borrow().clone();
-
-        if *cur_token.token_type() == TokenType::Literal
-            || *cur_token.token_type() == TokenType::Num
-        {
-            let value = cur_token.literal().to_string();
-            self.next_token();
-
-            return Some(value);
-        }
-
-        None
     }
 
     // Update the current token and move the position that in Lexer to next token.
---
+++ src/parser/parsing_func.rs
@@ -0,0 +1,262 @@
+use crate::token::token::TokenType;
+
+use super::{ast_node_trait::CommandAstNode, cmds_ast_node::ExeCommandAstNode, Parser};
+
+// Here are the parsing functions for parsing each command type
+// Due to the different nature of each command, they are separated into different functions
+impl Parser {
+    // Parse the ls command.
+    // The ls command has the following options:
+    // -l, --long: show the long format
+    // -a, --all: do not ignore entries starting with .
+    // -h, --human-readable: with -l and/or -s, print human readable sizes
+    // -r, --reverse: reverse order while sorting
+    // -t, --time: sort by modification time, newest first
+    // -s, --size: sort by file size, largest first
+    // --tree: show the directory tree
+    // --depth: show the directory tree with the specified depth
+    pub fn parse_ls_cmd(&self) -> Option<Box<dyn CommandAstNode>> {
+        // Build the exe command node.
+        let mut ls_cmd = ExeCommandAstNode::new(self.cur_token.borrow().clone());
+
+        self.next_token();
+
+        // Parse the parameters of the command.
+        let mut options: Vec<(String, String)> = Vec::new();
+        loop {
+            if *self.cur_token.borrow().token_type() == TokenType::Eof
+                || !(*self.cur_token.borrow().token_type() == TokenType::ShortParam
+                    || *self.cur_token.borrow().token_type() == TokenType::LongParam)
+            {
+                break;
+            }
+
+            let cur_token = self.cur_token.borrow().clone();
+            match cur_token.literal() {
+                "-l" | "--long" => {
+                    options.push(self.parse_option(false));
+                }
+                "-a" | "--all" => {
+                    options.push(self.parse_option(false));
+                }
+                "-h" | "--human-readable" => {
+                    options.push(self.parse_option(false));
+                }
+                "-r" | "--reverse" => {
+                    options.push(self.parse_option(false));
+                }
+                "-t" | "--time" => {
+                    options.push(self.parse_option(false));
+                }
+                "-s" | "--size" => {
+                    options.push(self.parse_option(false));
+                }
+                "--tree" => {
+                    options.push(self.parse_option(false));
+                }
+                "--depth" => {
+                    options.push(self.parse_option(true));
+                }
+                _ => {
+                    options.push(self.parse_option(false));
+                }
+            }
+        }
+        ls_cmd.set_options(options);
+
+        // Parse the paths of the ls command.
+        match self.parse_paths() {
+            Some(paths) => ls_cmd.set_values(paths),
+            None => (),
+        };
+
+        Some(Box::new(ls_cmd))
+    }
+
+    // Parse the grep command.
+    // The grep command has the following options:
+    // -i, --ignore-case: ignore case distinctions
+    // -v, --invert-match: select non-matching lines
+    // -c, --count: print only a count of matching lines per FILE
+    // -n, --line-number: print line number with output lines
+    pub fn parse_grep_cmd(&self) -> Option<Box<dyn CommandAstNode>> {
+        // Build the exe command node.
+        let mut grep_cmd: ExeCommandAstNode =
+            ExeCommandAstNode::new(self.cur_token.borrow().clone());
+
+        self.next_token();
+
+        // Parse the parameters of the command.
+        let mut options: Vec<(String, String)> = Vec::new();
+        loop {
+            if *self.cur_token.borrow().token_type() == TokenType::Eof
+                || !(*self.cur_token.borrow().token_type() == TokenType::ShortParam
+                    || *self.cur_token.borrow().token_type() == TokenType::LongParam)
+            {
+                break;
+            }
+
+            let cur_token = self.cur_token.borrow().clone();
+            match cur_token.literal() {
+                "-i" | "--ignore-case" => {
+                    options.push(self.parse_option(false));
+                }
+                "-v" | "--invert-match" => {
+                    options.push(self.parse_option(false));
+                }
+                "-c" | "--count" => {
+                    options.push(self.parse_option(false));
+                }
+                "-n" | "--line-number" => {
+                    options.push(self.parse_option(false));
+                }
+                _ => {
+                    options.push(self.parse_option(false));
+                }
+            }
+        }
+        grep_cmd.set_options(options);
+
+        // set the pattern of the grep command.
+        let pattern = match self.parse_pattern() {
+            Some(pattern) => pattern,
+            None => return None,
+        };
+        grep_cmd.add_value(pattern);
+
+        // Parse the paths of the ls command.
+        match self.parse_paths() {
+            Some(paths) => grep_cmd.set_values(paths),
+            None => {
+                self.collect_error("Grep command needs a path");
+            }
+        };
+
+        Some(Box::new(grep_cmd))
+    }
+
+    pub fn parse_cat_cmd(&self) -> Option<Box<dyn CommandAstNode>> {
+        todo!()
+    }
+
+    // Parse the parameters of the command.
+    // @param whether_parsing_value: whether the parser is parsing the value of the option.
+    pub fn parse_option(&self, whether_parsing_value: bool) -> (String, String) {
+        let option = self.cur_token.borrow().literal().to_string();
+        self.next_token();
+
+        // Parse the value of the option.
+        let mut value = String::from("");
+        if whether_parsing_value {
+            // Skip the assignment operator.
+            if *self.cur_token.borrow().token_type() == TokenType::Assignment {
+                self.next_token();
+            }
+
+            if *self.cur_token.borrow().token_type() == TokenType::Literal
+                || *self.cur_token.borrow().token_type() == TokenType::Num
+            {
+                value = self.cur_token.borrow().literal().to_string();
+                self.next_token();
+            }
+        }
+
+        (option, value)
+    }
+
+    // Parse the paths of the command.
+    pub fn parse_paths(&self) -> Option<Vec<String>> {
+        let mut paths: Vec<String> = Vec::new();
+
+        loop {
+            let cur_tok = self.cur_token.borrow().clone();
+            match *cur_tok.token_type() {
+                TokenType::Tilde
+                | TokenType::Literal
+                | TokenType::Num
+                | TokenType::Slash
+                | TokenType::Dot => {
+                    paths.push(self.parse_path().unwrap());
+                }
+                _ => break,
+            };
+
+            // Skip the comma and get next path.
+            // If the current token isn't comma, then break the loop.
+            if self.cur_token.borrow().clone().token_type() != &TokenType::Comma {
+                break;
+            }
+            self.next_token();
+        }
+
+        if paths.is_empty() {
+            None
+        } else {
+            Some(paths)
+        }
+    }
+
+    // Parse the path of the command.
+    fn parse_path(&self) -> Option<String> {
+        let mut path = String::from(self.cur_token.borrow().literal());
+        self.next_token();
+
+        loop {
+            if self.lexer.peek_token().is_none() {
+                break;
+            }
+
+            if *self.cur_token.borrow().token_type() == TokenType::Literal
+                || *self.cur_token.borrow().token_type() == TokenType::Num
+                || *self.cur_token.borrow().token_type() == TokenType::Slash
+                || *self.cur_token.borrow().token_type() == TokenType::Dot
+                || *self.cur_token.borrow().token_type() == TokenType::Tilde
+            {
+                path.push_str(self.cur_token.borrow().literal());
+                self.next_token();
+            } else {
+                break;
+            }
+        }
+
+        Some(path)
+    }
+
+    // Parse the matching rules of the 'Pattern matching' command.
+    fn parse_pattern(&self) -> Option<String> {
+        // If the current token is a double quotation mark, then the pattern is complete.
+        if *self.cur_token.borrow().token_type() == TokenType::Quote {
+            self.next_token();
+        } else {
+            self.collect_error("Missing pattern. You can use `\"` to quote the pattern.");
+            return None;
+        }
+
+        // Iterate through the subsequent tokens and add them to the current pattern
+        // until the condition is not met.
+        let mut pattern = String::from("");
+        loop {
+            if *self.cur_token.borrow().token_type() == TokenType::Literal
+                || *self.cur_token.borrow().token_type() == TokenType::Num
+                || *self.cur_token.borrow().token_type() == TokenType::Slash
+                || *self.cur_token.borrow().token_type() == TokenType::Dot
+                || *self.cur_token.borrow().token_type() == TokenType::Tilde
+            {
+                pattern.push_str(self.cur_token.borrow().literal());
+                self.next_token();
+            } else {
+                break;
+            }
+        }
+
+        // If the current token is a double quotation mark, then the pattern is complete.
+        if *self.cur_token.borrow().token_type() == TokenType::Quote {
+            self.next_token();
+        } else {
+            self.collect_error("Invalid pattern, missing right quotation mark.");
+            return None;
+        }
+
+        Some(pattern)
+    }
+}
